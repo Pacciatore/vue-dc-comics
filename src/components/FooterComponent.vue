39caf000ae448372b70e6764ce13ff2be90be913
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-23 05:02:39 +0200

    pulizia footer component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -63,31 +63,18 @@
 
         </div>
 
-        <!-- Bottom footer -->
-        <div class="bottom-footer py-4">
-            <div class="container d-flex justify-content-between">
-
-                <button class="sign-up-btn py-2 px-3 fs-4">Sign-Up Now!</button>
-
-                <div class="socials col-4 d-flex align-items-center justify-content-around">
-                    <div class="social-invite text-uppercase fs-3">Follow us</div>
-                    <a href="#" class="social-link"><img src="../assets/img/footer-facebook.png" alt=""></a>
-                    <a href="#" class="social-link"><img src="../assets/img/footer-twitter.png" alt=""></a>
-                    <a href="#" class="social-link"><img src="../assets/img/footer-youtube.png" alt=""></a>
-                    <a href="#" class="social-link"><img src="../assets/img/footer-pinterest.png" alt=""></a>
-                    <a href="#" class="social-link"><img src="../assets/img/footer-periscope.png" alt=""></a>
-                </div>
-
-            </div>
-        </div>
+        <!-- BottomFooter Component Reference -->
+        <BottomFooterComponent />
 
     </footer>
 </template>
 
 
 <script>
+import BottomFooterComponent from './BottomFooterComponent.vue';
 export default {
     name: "FooterComponent",
+    components: { BottomFooterComponent }
 }
 </script>
 
@@ -111,6 +98,8 @@ li:first-child {
     color: white;
     font-weight: 400;
     text-transform: uppercase;
+
+    cursor: default;
 }
 
 li a {
@@ -130,33 +119,5 @@ li a:hover {
     background-position: center;
     background-repeat: no-repeat;
     background-size: auto;
-}
-
-// Bottom footer
-.bottom-footer {
-    background-color: $tr-dc-bg-bottom-footer;
-}
-
-.sign-up-btn {
-    background-color: inherit;
-    color: white;
-
-    text-transform: uppercase;
-    border: 2px solid $tr-dc-blue;
-
-    transition: all 0.25s;
-}
-
-.sign-up-btn:hover {
-    color: $tr-dc-blue;
-    border-color: white;
-}
-
-.social-invite {
-    color: $tr-dc-blue;
-}
-
-.social-link:hover {
-    filter: brightness(50);
 }
 </style>
