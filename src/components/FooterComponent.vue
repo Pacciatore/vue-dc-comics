5930761619d21db31e9b5e249710ac519bac44c2
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-23 03:31:34 +0200

    bottom footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,12 @@
 <template>
     <footer class="fs-5">
+
+        <!-- Main footer -->
         <div class="container">
 
-            <div class="w-50 d-flex">
+            <div class="w-50 d-flex py-5">
 
-                <!-- Firt column -->
+                <!-- First column -->
                 <div class="col-4">
                     <ul>
                         <li class="fs-3">DC Comics</li>
@@ -56,6 +58,25 @@
 
             </div>
         </div>
+
+        <!-- Bottom footer -->
+        <div class="bottom-footer py-4">
+            <div class="container d-flex justify-content-between">
+
+                <button class="sign-up-btn py-2 px-3 fs-4">Sign-Up Now!</button>
+
+                <div class="socials col-4 d-flex align-items-center justify-content-around">
+                    <div class="social-invite text-uppercase fs-3">Follow us</div>
+                    <a href="#" class="social-link"><img src="../assets/img/footer-facebook.png" alt=""></a>
+                    <a href="#" class="social-link"><img src="../assets/img/footer-twitter.png" alt=""></a>
+                    <a href="#" class="social-link"><img src="../assets/img/footer-youtube.png" alt=""></a>
+                    <a href="#" class="social-link"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+                    <a href="#" class="social-link"><img src="../assets/img/footer-periscope.png" alt=""></a>
+                </div>
+
+            </div>
+        </div>
+
     </footer>
 </template>
 
@@ -68,6 +89,9 @@ export default {
 
 
 <style scoped lang="scss">
+@import './variables.scss';
+
+// Main footer
 footer {
     background-image: url(../assets/img/footer-bg.jpg);
     background-size: cover;
@@ -90,5 +114,26 @@ li:first-child {
 li {
     color: #ffffff5f;
     font-weight: 300;
+}
+
+// Bottom footer
+.bottom-footer {
+    background-color: $tr-dc-bg-bottom-footer;
+}
+
+.sign-up-btn {
+    background-color: inherit;
+    color: white;
+
+    text-transform: uppercase;
+    border: 2px solid $tr-dc-blue;
+}
+
+.social-invite {
+    color: $tr-dc-blue;
+}
+
+.social-link:hover {
+    filter: brightness(50);
 }
 </style>
