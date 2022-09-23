b38dacc4b0d422fc00f71306d701b0ad553a7a72
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-23 04:42:48 +0200

    prefooter area in main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,52 @@
 <template>
     <main>
-        Questo è il corpo della pagina
+
+        <!-- Main content -->
+        <div class="content container fs-1">
+            -->CONTENT GOES HERE&lt;--
+        </div>
+
+        <!-- Pre Footer -->
+        <div class="pre-footer">
+
+            <div class="container d-flex py-5 justify-content-around align-items-center">
+
+                <!-- Digital Comics -->
+                <div>
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+                    <span class="fs-5">Digital Comics</span>
+                </div>
+
+                <!-- DC Merchandise -->
+                <div>
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-merchandise.png"
+                        alt="comics-merchandise">
+                    <span class="fs-5">DC Merchandise</span>
+                </div>
+
+                <!-- Subscription -->
+                <div>
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-subscriptions.png" alt="subscriptions">
+                    <span class="fs-5">Subscription</span>
+                </div>
+
+                <!-- Comic Shop Locator -->
+                <div>
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-shop-locator.png" alt="shop-locator">
+                    <span class="fs-5">Comic Shop Locator</span>
+                </div>
+
+                <!-- DC Power Visa -->
+                <div>
+                    <img class="pre-footer-icon svg" src="../assets/img/buy-dc-power-visa.svg" alt="dc-power-visa">
+                    <span class="fs-5">DC Power Visa</span>
+                </div>
+
+            </div>
+
+        </div>
+
+
     </main>
 </template>
 
@@ -13,9 +59,38 @@ export default {
 
 
 <style scoped lang="scss">
+@import './variables.scss';
+
 main {
     min-height: 500px;
     background-color: black;
     color: white;
+
+    // For pre footer temporary position
+    position: relative;
+}
+
+// Top footer
+.pre-footer {
+    background-color: $tr-dc-blue;
+
+    // For pre footer temporary position
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+
+.pre-footer span {
+    text-transform: uppercase;
+    font-weight: 300;
+}
+
+.pre-footer-icon {
+    height: 60px;
+    padding-right: 10px;
+}
+
+.pre-footer-icon.svg {
+    width: 70px;
 }
 </style>
