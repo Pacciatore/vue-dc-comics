bbec63564406c99f4345b0639b1ae811ebeeb219
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-23 04:53:10 +0200

    added links prefooter

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -13,33 +13,45 @@
 
                 <!-- Digital Comics -->
                 <div>
-                    <img class="pre-footer-icon" src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
-                    <span class="fs-5">Digital Comics</span>
+                    <a href="#">
+                        <img class="pre-footer-icon" src="../assets/img/buy-comics-digital-comics.png"
+                            alt="digital-comics">
+                        <span class="fs-5">Digital Comics</span>
+                    </a>
                 </div>
 
                 <!-- DC Merchandise -->
                 <div>
-                    <img class="pre-footer-icon" src="../assets/img/buy-comics-merchandise.png"
-                        alt="comics-merchandise">
-                    <span class="fs-5">DC Merchandise</span>
+                    <a href="#">
+                        <img class="pre-footer-icon" src="../assets/img/buy-comics-merchandise.png"
+                            alt="comics-merchandise">
+                        <span class="fs-5">DC Merchandise</span>
+                    </a>
                 </div>
 
                 <!-- Subscription -->
                 <div>
-                    <img class="pre-footer-icon" src="../assets/img/buy-comics-subscriptions.png" alt="subscriptions">
-                    <span class="fs-5">Subscription</span>
+                    <a href="#">
+                        <img class="pre-footer-icon" src="../assets/img/buy-comics-subscriptions.png"
+                            alt="subscriptions">
+                        <span class="fs-5">Subscription</span>
+                    </a>
                 </div>
 
                 <!-- Comic Shop Locator -->
                 <div>
-                    <img class="pre-footer-icon" src="../assets/img/buy-comics-shop-locator.png" alt="shop-locator">
-                    <span class="fs-5">Comic Shop Locator</span>
+                    <a href="#">
+                        <img class="pre-footer-icon" src="../assets/img/buy-comics-shop-locator.png" alt="shop-locator">
+                        <span class="fs-5">Comic Shop Locator</span>
+                    </a>
                 </div>
 
                 <!-- DC Power Visa -->
                 <div>
-                    <img class="pre-footer-icon svg" src="../assets/img/buy-dc-power-visa.svg" alt="dc-power-visa">
-                    <span class="fs-5">DC Power Visa</span>
+                    <a href="#">
+                        <img class="pre-footer-icon svg" src="../assets/img/buy-dc-power-visa.svg" alt="dc-power-visa">
+                        <span class="fs-5">DC Power Visa</span>
+                    </a>
                 </div>
 
             </div>
@@ -78,6 +90,11 @@ main {
     position: absolute;
     bottom: 0;
     width: 100%;
+}
+
+.pre-footer a {
+    color: inherit;
+    text-decoration: none;
 }
 
 .pre-footer span {
