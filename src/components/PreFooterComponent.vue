42f411513a20e7ac93bb22b130c0fb2969b36a8d
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-23 04:55:57 +0200

    creazione prefooter component

---
+++ src/components/PreFooterComponent.vue
@@ -0,0 +1,91 @@
+<template>
+
+    <!-- Pre Footer -->
+    <div class="pre-footer">
+
+        <div class="container d-flex py-5 justify-content-around align-items-center">
+
+            <!-- Digital Comics -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+                    <span class="fs-5">Digital Comics</span>
+                </a>
+            </div>
+
+            <!-- DC Merchandise -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-merchandise.png"
+                        alt="comics-merchandise">
+                    <span class="fs-5">DC Merchandise</span>
+                </a>
+            </div>
+
+            <!-- Subscription -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-subscriptions.png" alt="subscriptions">
+                    <span class="fs-5">Subscription</span>
+                </a>
+            </div>
+
+            <!-- Comic Shop Locator -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="../assets/img/buy-comics-shop-locator.png" alt="shop-locator">
+                    <span class="fs-5">Comic Shop Locator</span>
+                </a>
+            </div>
+
+            <!-- DC Power Visa -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon svg" src="../assets/img/buy-dc-power-visa.svg" alt="dc-power-visa">
+                    <span class="fs-5">DC Power Visa</span>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'PreFooterComponent'
+}
+</script>
+
+<style lang="scss">
+@import './variables.scss';
+
+.pre-footer {
+    background-color: $tr-dc-blue;
+
+    // For pre footer temporary position
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+
+.pre-footer a {
+    color: inherit;
+    text-decoration: none;
+}
+
+.pre-footer span {
+    text-transform: uppercase;
+    font-weight: 300;
+}
+
+.pre-footer-icon {
+    height: 60px;
+    padding-right: 10px;
+}
+
+.pre-footer-icon.svg {
+    width: 70px;
+}
+</style>
