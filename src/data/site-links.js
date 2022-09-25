acd0b7cfd7203c32f320905d541754ebe2a235be
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-25 20:30:29 +0200

    aggiunta in /data di .js con array di link

---
+++ src/data/site-links.js
@@ -0,0 +1,23 @@
+const dcNavLinks = {
+    title: 'DC Comics',
+    elements: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+};
+
+const dcShops = {
+    title: 'Shop',
+    elements: ['Shop DC', 'Shop DC Collectibles']
+};
+
+const dcLegals = {
+    title: 'DC',
+    elements: ['Terms of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshop', 'CPSC Certificates', 'Ratings', 'Shop Heplp', 'Contact Us']
+};
+
+const sitesList = {
+    title: 'Sites',
+    elements: ['DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+}
+
+// Possibile implementazione: correlare un link ad ogni elemento
+
+export { dcNavLinks, dcShops, dcLegals, sitesList }
