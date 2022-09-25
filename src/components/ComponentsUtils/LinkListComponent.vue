433d2d6426982af0959fdffbaeabf9c85181c67a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-25 17:37:55 +0200

    Creazione componente x creazione liste

---
+++ src/components/ComponentsUtils/LinkListComponent.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <ul>
+        <li class="fs-3">{{ listArray.title }}</li>
+        <li v-for="liElement in listArray.elements" :key="liElement">
+            <a href="#">{{ liElement }}</a>
+        </li>
+    </ul>
+
+</template>
+
+<script>
+export default {
+    name: 'LinkList',
+    props: {
+        listArray: Object
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
